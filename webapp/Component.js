5d51f3420fdcb10aa50c91ea3c5c5735a8bd85b1
SergeyPsh <sergejpsh@gmail.com>
2020-04-15 21:25:38 +0000

    16.04.20 w2u6

--- webapp/Component.js
+++ webapp/Component.js
@@ -5,13 +5,16 @@ sap.ui.define([
 
 	return UIComponent.extend("opensap.myapp.Component", {
 
-		metadata : {
+		metadata: {
 			manifest: "json"
 		},
 
-		init : function () {
+		init: function () {
 			// call the init function of the parent
 			UIComponent.prototype.init.apply(this, arguments);
+
+			// used only for this lessons to show the request individually...
+			this.getModel().setUseBatch(false);
 
 			// additional initialization can be done here
 		}
