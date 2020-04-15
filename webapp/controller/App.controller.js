a1520f2d46ca7d3f0c38b87541e78bffdcce1358
SergeyPsh <sergejpsh@gmail.com>
2020-04-15 12:47:42 +0000

    15.04.20 w2u5

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -37,6 +37,26 @@ sap.ui.define([
 			// apply filter. an empty filter array simply removes the filter
 			// which will make all entries visible again
 			oBinding.filter(aFilter);
-		}
+		},
+		
+		onItemSelected: function(oEvent) {
+		var oSelectedItem = oEvent.getSource();
+		var oContext = oSelectedItem.getBindingContext();
+		var sPath = oContext.getPath();
+		var oProductDetailPanel = this.byId("productDetailsPanel");
+
+		oProductDetailPanel.bindElement({ path: sPath });
+		this.byId("productDetailsPanel").setVisible(true); 
+	}
+	// ,
+	
+	// 	onItemSelected : function (oEvent){
+	// 		var oSelectedItem = oEvent.getParameter("listItem");
+	// 		var oContext = oSelectedItem.getBindingContext();
+	// 		var sPath = oContext.getPath();
+	// 		var oPanel = this.byId("productDatailsPanel");
+	// 		oPanel.bindElement({path: sPath});
+	// 		oPanel.setVisible(true);
+	// 	}
 	});
 });
